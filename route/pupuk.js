7a4fda989902c5b0e6bb19ec277afe76d5ffc9fc
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-11-23 20:38:47 +0700

    membuat array pupuk

---
+++ route/pupuk.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+const pupuk = [
+  { id: 1, jenisPupuk: "Organik", namaPupuk: "Kompos", deskripsi: "Pupuk alamiyang terbuat dari bahan organik." },
+  { id: 2, jenisPupuk: "Kimia", namaPupuk: "Urea", deskripsi: "Pupuk kimia untuk meningkatkan hasil panen." },
+];
+
