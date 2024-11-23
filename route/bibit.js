e4547e918a8a0cc468899e3bc479e8812406f4c8
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-11-23 20:17:20 +0700

    membuat array bibit

---
+++ route/bibit.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+const bibit = [
+    { id: 1, namaBibit: "Mangga", jenisTanaman: "Buah", harga: 10000 },
+    { id: 2, namaBibit: "Padi", jenisTanaman: "Pangan", harga: 5000 },
+];
+
