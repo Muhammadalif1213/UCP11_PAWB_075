1af52b5cccf16bbd1260b70c226e6e43bea4ee73
Muhammadalif1213 <muhammadalif1213@gmail.com>
2024-11-23 20:47:25 +0700

    mengimport bibit.js dan pupuk.js di index

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import bodyParser from "body-parser";
 import express from "express";
+import bibitRoute from "./route/bibit.js";
+import pupukRoute from "./route/pupuk.js";
 
 const app = express();
 const port = 8000;
 
 app.use(bodyParser.json());
+
 
 app.get("/", (req, res) => {
     console.log("GET ROUTE");
